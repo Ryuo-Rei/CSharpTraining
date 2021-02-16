ad14f7a8ef6a67e9f83b99d749e9ee9545665508
Ryuo-Rei <70551513+Ryuo-Rei@users.noreply.github.com>
2021-02-16 09:21:19 +0900

    TextDifferentHunt更新
    TextDifferentHunt
    変更

--- TextDifferentHunt/TextDifferentHunt/Form1.cs
+++ TextDifferentHunt/TextDifferentHunt/Form1.cs
@@ -47,10 +47,17 @@ namespace TextDifferentHunt
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
-            Form2 form2 = new Form2(this);
-            form2.Size = new Size(int.Parse(textPictureWidth.Text), int.Parse(textPictureHeight.Text));
-            this.TopMost = false;
-            form2.Show();
+            if (mainPictureBox.Image == null)
+            {
+                MessageBox.Show("画像イメージがありません。画像を生成してください。");
+            }
+            else
+            {
+                Form2 form2 = new Form2(this);
+                form2.Size = new Size(int.Parse(textPictureWidth.Text), int.Parse(textPictureHeight.Text));
+                this.TopMost = false;
+                form2.Show();
+            }
         }
 
         private void backGroundColorPanel_Click(object sender, EventArgs e)
@@ -73,10 +80,19 @@ namespace TextDifferentHunt
 
         private void buttonCreate_Click(object sender, EventArgs e)
         {
-            if(IsNumber() == true)
+            if(IsNumber() == false)
+            {
+                MessageBox.Show("画像サイズを数字で入力してください。");
+            }
+
+            else if (String.IsNullOrWhiteSpace(textString.Text) == true)
+            {
+                MessageBox.Show("生成する文字列を入力してください。");
+            }
+            else
             {
                 if (float.Parse(textPictureWidth.Text) > 0
-                    && float.Parse(textPictureHeight.Text) > 0)
+                                    && float.Parse(textPictureHeight.Text) > 0)
                 {
                     Brush textColorBrush = new SolidBrush(textColorPanel.BackColor);
                     Brush backColorBrush = new SolidBrush(backGroundColorPanel.BackColor);
@@ -95,14 +111,8 @@ namespace TextDifferentHunt
                 }
                 else
                 {
-                    MessageBox.Show("0より大きい数字を入力してください。");
+                    MessageBox.Show("画像サイズは0より大きい数字を入力してください。");
                 }
-            }
-
-            else
-            {
-                MessageBox.Show("数字を入力してください。");
-
             }
         }
 
